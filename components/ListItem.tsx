5e49d58ddf0ffbaf3ddcbc6df1e1c7aedeb502e1
Hendra <muhhendrasaputra93@gmail.com>
2023-07-02 14:06:11 +0800

    v2

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -4,6 +4,9 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { FaPlay } from 'react-icons/fa';
 
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+
 interface ListItemProps {
   image: string;
   name: string;
@@ -16,9 +19,14 @@ const ListItem: React.FC<ListItemProps> = ({
   href
 }) => {
   const router = useRouter();
+  const authModal = useAuthModal();
+  const { user } = useUser();
   
   const onClick = () => {
-    // Skd
+    if (!user) {
+      return authModal.onOpen();
+    }
+    
     router.push(href);
   }
   
